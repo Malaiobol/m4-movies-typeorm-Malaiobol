61fb3eed9bbe1eac9e1dde5676dedb6df0120d61
Edmilson <krakadianbill@gmail.com>
2023-02-26 04:13:56 -0300

    feat: post controller added

---
+++ src/controllers/movies.controllers.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express'
+
+const createMovieController = async (req: Request, resp: Response) =>{
+
+}
+
+export {
+    createMovieController
+}
--- src/routes/movies.routes.ts
+++ src/routes/movies.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import { createMovieController } from '../controllers/movies.controllers';
 import ensureMovieDataIsValid from '../middlewares/ensureDataIsValid.middleware';
 import { movieSchema } from '../schemas/movies.schemas';
 
 const movieRouts: Router = Router()
 
-movieRouts.post('', ensureMovieDataIsValid(movieSchema))
+movieRouts.post('', ensureMovieDataIsValid(movieSchema), createMovieController)
 
 export default movieRouts
