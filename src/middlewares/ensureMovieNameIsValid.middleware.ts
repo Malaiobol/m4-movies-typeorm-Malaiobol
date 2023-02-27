487d6c7da75b7cc84a579e83dc0f0a3f33db07f0
Edmilson <krakadianbill@gmail.com>
2023-02-27 16:23:16 -0300

    fix: uniqueName middleware now make the verification correctly

--- src/middlewares/ensureMovieNameIsValid.middleware.ts
+++ src/middlewares/ensureMovieNameIsValid.middleware.ts
@@ -7,19 +7,17 @@ import { AppError } from '../errors'
 const ensureMovieIsUnique = async (req: Request, resp: Response, next:NextFunction): Promise<void> =>{
     const moviesRepository: Repository<Movie> = AppDataSource.getRepository(Movie)
 
-    const findMovie = moviesRepository.findOne({
+    const findMovie = await moviesRepository.findOne({
         where:{ 
             name: req.body.name
         }
     })
 
-    console.log(findMovie)
-
     if(!findMovie){
       return next()  
-    } 
-        
-    throw new AppError('Movie already exists.', 409)
+    } else{
+        throw new AppError('Movie already exists.', 409)
+    }
 }
 
 export default ensureMovieIsUnique
